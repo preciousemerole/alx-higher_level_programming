ab3e89e5ef86c8059dde8a46715331b61d0908c1
Precious Emerole <preciousemerole@gmail.com>
2022-12-13 01:14:31 -0800

    reduced length of comment in code

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 # 101-remove_char_at.py
 
 def remove_char_at(str, n):
-    """creates a copy of the string, removing the character at the position n"""
+    """creates a copy of the string, removes the character at the position n"""
     if n < 0:
         return (str)
     return (str[:n] + str[n+1:])
