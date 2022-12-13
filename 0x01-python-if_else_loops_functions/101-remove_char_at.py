35fb7959bc5d44f6f3a1a4ddd87656c9ad994283
Precious Emerole <preciousemerole@gmail.com>
2022-12-13 01:12:11 -0800

    completed task 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 101-remove_char_at.py
+
+def remove_char_at(str, n):
+    """creates a copy of the string, removing the character at the position n"""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
